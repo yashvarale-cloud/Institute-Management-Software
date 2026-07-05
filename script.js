c57fddbb9af44fc41fb3e06a60cbd934c0a008cd
yashvarale-cloud <yashvarale@gmail.com>
2026-07-05 23:11:54 +0530

    new file

--- script.js
+++ script.js
@@ -19,6 +19,8 @@
   document.querySelectorAll('.nav-item').forEach(btn => {
     btn.addEventListener('click', () => goTo(btn.dataset.target));
   });
+
+  
   document.querySelectorAll('.switch').forEach(sw => {
     sw.addEventListener('click', () => sw.classList.toggle('on'));
   });
